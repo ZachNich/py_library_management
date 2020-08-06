81049bf54749726102dfbb86b07512e6ff373240
Zach Nicholson <raiste.doctow@gmail.com>
2020-08-06 11:52:51 -0500

    finished up to links

---
+++ database.session.sql
@@ -0,0 +1,12 @@
+INSERT INTO libraryapp_book
+(title, isbn_number, year_published, location_id, author, librarian_id)
+VALUES
+('The Golem and the Jinni', '8592475822', 2013, 1, 'Helene Wecker', 2);
+INSERT INTO libraryapp_book
+(title, isbn_number, year_published, location_id, author, librarian_id)
+VALUES
+('Frankenstein', '6664201337', 1970, 1, 'Mary Shelley', 3);
+INSERT INTO libraryapp_book
+(title, isbn_number, year_published, location_id, author, librarian_id)
+VALUES
+('A Game of Thrones', '1234567890', 1990, 1, 'George R. R. Martin', 4);
