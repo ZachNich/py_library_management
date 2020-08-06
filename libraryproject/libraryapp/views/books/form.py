9d13755e46660e684f3081038c0b77b655caa192
Zach Nicholson <raiste.doctow@gmail.com>
2020-08-06 13:20:13 -0500

    delete and edit finished

--- libraryproject/libraryapp/views/books/form.py
+++ libraryproject/libraryapp/views/books/form.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from libraryapp.models import Book
 from libraryapp.models import Library
+from .details import get_book
 from ..connection import Connection
 
 
@@ -27,6 +28,21 @@ def book_form(request):
         libraries = get_libraries()
         template = 'books/form.html'
         context = {
+            'all_libraries': libraries
+        }
+
+        return render(request, template, context)
+
+@login_required
+def book_edit_form(request, book_id):
+
+    if request.method == 'GET':
+        book = get_book(book_id)
+        libraries = get_libraries()
+
+        template = 'books/form.html'
+        context = {
+            'book': book,
             'all_libraries': libraries
         }
 
